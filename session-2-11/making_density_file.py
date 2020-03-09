08a74ad5305d7c574439a0d33ae726dac88aa759
khostetterhabib22@riverdale.edu <khostetterhabib22@riverdale.edu>
2020-03-09 11:56:07 -0400

    add changes

--- session-2-11/making_density_file.py
+++ session-2-11/making_density_file.py
@@ -2,7 +2,7 @@ from netCDF4 import Dataset
 import numpy as np
 import seawater as sw
 
-dataset = Dataset(r'/Users/brownscholar/Desktop/Internships/ocean-ml/session-2-11/dataset-armor-3d-rep-weekly_1574699840388.nc')
+dataset = Dataset(r'/Users/brownscholar/Desktop/Internships/dataset-armor-3d-rep-weekly_1574699840388.nc')
 
 # first step: import the data and get temp, salinity pressure
 temperature = dataset['to']
@@ -31,11 +31,20 @@ for i in range(0,31):
 
 density = sw.dens(salinity[:],temperature[:],pressure_3d)
 
-print(density)
+density = density -1000
+
+print(density.shape)
 
 	
 
-
+for i in range(0,1356):
+	my_file = open("density_"+str(i)+".txt","w")
+	for j in range(0,31):
+		for k in range(0,80):
+			for w in range(0,27):
+				my_file.write(str(density[i,j,k,w]))
+				my_file.write("\n")
+	my_file.close()
 
 # to do: 
 
--- session-3-10/color_plot.py
+++ session-3-10/color_plot.py
@@ -4,8 +4,8 @@ from netCDF4 import Dataset
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
-w_file = '/path/to/file' #fix this
-original_data = #open netcdf file
+w_file = open('/Users/brownscholar/Desktop/Internships/ocean-ml/session-3-10/ss1/ss1a2ww.gr',"r") #fix this
+#original_data = #open netcdf file
 
 #get latitude and longitude from netcdf file: 
 # here:
@@ -16,11 +16,19 @@ num_lon = 27
 levels = 30
 
 # make empty numpy array of lat, lon depth shape for storing w:
-#
+w_array = np.zeros((num_lat,num_lon,levels))
 
 # use a loop to read w_file into the variable 
 #(skip first two lines of the file)
 
+w_file.readline()
+w_file.readline()
+
+for i in range(0,levels):
+	for j in range(0, num_lon):
+		for k in range(0,num_lat):
+			temp = w_file.readline()
+			w_array[k,j,i] = temp
 
 
 #this stuff defines the colorspace (we can google colormaps to learn more if we want to)
@@ -31,6 +39,9 @@ newcolors = np.vstack((top(np.linspace(0, 1, 128)),
                        bottom(np.linspace(0, 1, 128))))
 newcmp = ListedColormap(newcolors, name='RedBlue')
 
+plt.pcolormesh(w_array[2:78,2:25,0], cmap = newcmp)
+
+plt.show()
 
 #now select one level of the data (surface level):
 
